92376536c0f61a0f654b12a1b210bbf06bbfd7f3
Adam <adam.stammer@go.winona.edu>
2020-04-20 11:15:04 -0500

    replacing master with fork

--- TFTP/src/edu/dmas/tftp/Client.java
+++ TFTP/src/edu/dmas/tftp/Client.java
@@ -1,5 +1,12 @@
 package edu.dmas.tftp;
 
+import java.awt.Frame;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
 public class Client {
 	
 	
@@ -10,9 +17,64 @@ public class Client {
 		//initializer
 	}
 	
-	public void requestFile() {
+	public void requestFile(String addr, String rType, String transferMode,String fileName) {
 		//this will be called from TFTP.java as the 'run' method
 		//setup options and special varibles beforehand, then call this method
+		try {
+			DatagramSocket socket = new DatagramSocket();
+			
+			
+			byte[] opcode;
+			byte[] fname = fileName.getBytes();
+			byte[] tmode = transferMode.getBytes();
+			
+			
+			if(rType.equalsIgnoreCase("Get")) {
+				opcode = new byte[] {0,1};
+			}
+			else {
+				opcode = new byte[]{0,2};
+			}
+			
+			
+			byte[] buf = new byte[4 + fname.length + tmode.length];
+			
+			for(int i =0; i < buf.length; i++) {
+				
+				if(i < 2) {
+					
+				}
+				else if(i > 1 && i <= fname.length + 1) {
+					
+				}
+				else if(i == fname.length + 2) {
+					
+					
+					
+					
+				}
+				
+				
+			}
+			
+			
+			InetAddress address = InetAddress.getByName(addr);
+			
+			
+			
+			
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
 	}
 
 }
