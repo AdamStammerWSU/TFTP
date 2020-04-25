7426d97d50eea3a0a6363166b066d1877a542513
Adam <adam.stammer@go.winona.edu>
2020-04-25 10:37:03 -0500

    request buffer looks good!

--- TFTP/src/edu/dmas/tftp/Client.java
+++ TFTP/src/edu/dmas/tftp/Client.java
@@ -4,6 +4,7 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
 
 public class Client {
 
@@ -22,13 +23,21 @@ public class Client {
 		}
 	};
 
-	private String host, transferMode;
+	private String address, transferMode;
 	private boolean getting;
+	private InetAddress host;
 
 	public Client(String addr, boolean getting, String transferMode) {
-		this.host = addr;
+		this.address = addr;
 		this.transferMode = transferMode;
 		this.getting = getting;
+
+		try {
+			host = InetAddress.getByName(address);
+		} catch (UnknownHostException e) {
+			System.out.println("Failed to resolve hostname.");
+			TFTP.exit(false);
+		}
 	}
 
 	public boolean requestFile(String source, String destination) {
@@ -55,6 +64,9 @@ public class Client {
 			// this "should" concatenate all of the pieces together for the get request
 			byte[] buf = RQconcat(opcode, fname, tmode);
 
+			// debug check of the byte array, it matches wireshark capture
+			System.out.println(bytesToHex(buf));
+
 //			byte[] buf = new byte[4 + fname.length + tmode.length];
 //			for (int i = 0; i < buf.length; i++) {
 //				if (i < 2) {
@@ -63,14 +75,7 @@ public class Client {
 //				}
 //			}
 
-			
-			
-			InetAddress address = InetAddress.getByName(host);
-
 		} catch (SocketException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -98,5 +103,18 @@ public class Client {
 		System.arraycopy(a, 0, c, 0, a.length);
 		System.arraycopy(b, 0, c, a.length, b.length);
 		return c;
+	}
+
+	// this purely a debug method. delete before turn in
+	private static final byte[] HEX_ARRAY = "0123456789ABCDEF".getBytes();
+
+	public static String bytesToHex(byte[] bytes) {
+		byte[] hexChars = new byte[bytes.length * 2];
+		for (int j = 0; j < bytes.length; j++) {
+			int v = bytes[j] & 0xFF;
+			hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+			hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+		}
+		return new String(hexChars, StandardCharsets.UTF_8);
 	}
 }
